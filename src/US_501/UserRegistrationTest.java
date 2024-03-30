e4a7714324a271cd51a0537e0021c57cc858f6a2
ozcansayis <ozcansayis@gmail.com>
2024-03-31 02:58:10 +0300

    Priority and a parameter added. Credentials updated, and final softAssert fixed.

--- src/US_501/UserRegistrationTest.java
+++ src/US_501/UserRegistrationTest.java
@@ -4,33 +4,32 @@ import POM.*;
 import Utility.BaseDriverParameter;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.*;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
-import java.time.Duration;
 
 public class UserRegistrationTest extends BaseDriverParameter {
-    @Test(groups = { "Smoke", "Regression"})
-    public void TC_501() {
-
+    @Test(groups = {"Smoke", "Regression"}, priority = 1)
+    @Parameters("email")
+    public void TC_501(String email) {
         LoginContent lc = new LoginContent();
-        HomePageContent hpc = new HomePageContent();
         SoftAssert softAssert = new SoftAssert();
-        WebDriverWait wait = new WebDriverWait(BaseDriverParameter.driver, Duration.ofSeconds(5));
-        myClick(hpc.registerbuton);
-        softAssert.assertTrue(hpc.registerbuton.isEnabled(), "Register button is not clickable");
-        mySendKeys(hpc.fistName, "Test");
-        mySendKeys(hpc.lastName, "Nomads");
-        mySendKeys(hpc.email, "testnomads01@gmail.com");
-        mySendKeys(hpc.password, "TOtechno1");
-        mySendKeys(hpc.ConfirmPassword, "TOtechno1");
+
+        myClick(lc.registerbuton);
+        softAssert.assertTrue(lc.registerbuton.isEnabled(), "Register button is not clickable!");
+        mySendKeys(lc.fistName, "Test");
+        mySendKeys(lc.lastName, "Nomads");
+        mySendKeys(lc.email, email);
+        mySendKeys(lc.password, "testnomads");
+        mySendKeys(lc.ConfirmPassword, "testnomads");
         Select dayDropdown = new Select(driver.findElement(By.name("DateOfBirthDay")));
         dayDropdown.selectByValue("5");
         Select monthDropdown = new Select(driver.findElement(By.name("DateOfBirthMonth")));
         monthDropdown.selectByVisibleText("May");
         Select yearDropdown = new Select(driver.findElement(By.name("DateOfBirthYear")));
         yearDropdown.selectByValue("2005");
-        myClick(hpc.registerButton);
-        softAssert.assertNotNull(hpc.successMessage, "Registration message should be displayed.");
+        myClick(lc.registerButton);
+        softAssert.assertTrue(lc.successMessage.getText().equals("Your registration completed"), "Registration message should be displayed!");
+        softAssert.assertAll();
     }
 
 }
