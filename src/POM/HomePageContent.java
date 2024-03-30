e633ad36da2676bee686e1ab315d1cae849fdf0e
ozcansayis <ozcansayis@gmail.com>
2024-03-30 06:28:38 +0300

    Gift Cards elements added.

--- src/POM/HomePageContent.java
+++ src/POM/HomePageContent.java
@@ -4,6 +4,8 @@ import Utility.BaseDriverParameter;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.*;
 
+import java.util.List;
+
 public class HomePageContent {
     public HomePageContent() {
         PageFactory.initElements(BaseDriverParameter.driver, this);
@@ -11,4 +13,43 @@ public class HomePageContent {
 
     @FindBy(linkText = "Log out")
     public WebElement logoutButton;
+
+    @FindBy(css = ".message")
+    public WebElement message;
+
+    @FindBy(css = "[id*='add-to-cart-button-']")
+    public WebElement addToCart;
+
+    @FindBy(css = ".cart-label")
+    public WebElement cart;
+
+    @FindBy(css = ".product-name")
+    public List<WebElement> cartItems;
+
+    // Gift Cards elements.
+    @FindBy(css = "[class='top-menu notmobile']>:nth-child(7)")
+    public WebElement giftCardsButton;
+
+    @FindBy(css = ".product-title>a")
+    public List<WebElement> giftCards;
+
+    @FindBy(css = ".recipient-name")
+    public WebElement recipientName;
+
+    @FindBy(css = ".recipient-email")
+    public WebElement recipientEmail;
+
+    @FindBy(css = "[class^='sender-name']")
+    public WebElement yourName;
+
+    @FindBy(css = ".sender-email")
+    public WebElement yourEmail;
+
+    @FindBy(css = "[class='bar-notification error']")
+    public WebElement giftError;
+
+    @FindBy(css = "[class='bar-notification success']")
+    public WebElement giftSuccess;
+
+
 }
