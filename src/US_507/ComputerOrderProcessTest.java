61ddb261efc95452ee7da2c1c6fdf7a03381492e
umiteser <eser_umit@hotmail.com>
2024-03-30 22:42:26 +0300

    RAM select is created.

--- src/US_507/ComputerOrderProcessTest.java
+++ src/US_507/ComputerOrderProcessTest.java
@@ -1,17 +1,43 @@
 package US_507;
 
 import POM.HomePageContent;
+import POM.ParentPage;
 import Utility.BaseDriverParameter;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 public class ComputerOrderProcessTest extends BaseDriverParameter {
-    @Test
+    @Test(groups = {"UITesting","TAB Menu","Order"})
     public void TC_507(){
 
         HomePageContent hp=new HomePageContent();
+        ParentPage pp=new ParentPage();
         myClick(hp.computers);
         myClick(hp.desktops);
         myClick(hp.buildOwnComputer);
+        Select ramSelect=new Select(hp.ramChoice);
+        List<WebElement> ram=ramSelect.getOptions();
+        int ramNo=ParentPage.RandomGenerator(ram.size()-1,1);
+        ramSelect.selectByIndex(ramNo);
+
+
+
+
+
+
+
+
+
+
+
+
+        Assert.assertEquals(hp.addToChartError.getText(), "The product has been added to your shopping cart. ", "Please select RAM or HDD");
 
 
 
