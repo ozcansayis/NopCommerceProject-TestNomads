2f65c455277cc7a0885c33fcc27d97f560b6d695
ozcansayis <ozcansayis@gmail.com>
2024-03-31 02:56:26 +0300

    Priority added, and credentials updated.

--- src/US_502/UserLoginTest.java
+++ src/US_502/UserLoginTest.java
@@ -6,14 +6,14 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class UserLoginTest extends BaseDriverParameter {
-    @Test(groups = {"LoginTest", "Smoke"})
+    @Test(groups = {"LoginTest", "Smoke"}, priority = 2)
     public void TC_502() {
         LoginContent lc = new LoginContent();
         HomePageContent hpc = new HomePageContent();
         myClick(lc.loginButton);
         Assert.assertEquals(lc.loginPageTitle.getText(), "Welcome, Please Sign In!", "Log in page could not be reached.");
-        mySendKeys(lc.email, "technodeneme007@gmail.com");
-        mySendKeys(lc.password, "12345Aa.");
+        mySendKeys(lc.email, "testnomads01@gmail.com");
+        mySendKeys(lc.password, "TOtechno1");
         myClick(lc.loginButton2);
         Assert.assertEquals(hpc.myAccount.getText(), "My account", "Failed entry.");
     }
