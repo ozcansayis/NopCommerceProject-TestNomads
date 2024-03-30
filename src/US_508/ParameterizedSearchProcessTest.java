7891836dad55f464b1f298ff4bb22d5a8869f049
Ayse Ihtiyar <ayseeihtiyar@gmail.com>
2024-03-30 17:52:28 +0300

    US_508 is done.

--- src/US_508/ParameterizedSearchProcessTest.java
+++ src/US_508/ParameterizedSearchProcessTest.java
@@ -1,10 +1,26 @@
 package US_508;
 
+import POM.HomePageContent;
+import POM.LoginContent;
+import Utility.BaseDriverParameter;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
-public class ParameterizedSearchProcessTest {
-    @Test
-    public void TC_508(){
 
+public class ParameterizedSearchProcessTest extends BaseDriverParameter {
+    @Test(groups = {"UITesting", "Search", "Smoke", "Regression"})
+    @Parameters("searchText")
+    public void TC_508(String text) {
+        LoginContent lc = new LoginContent();
+        HomePageContent hpc = new HomePageContent();
+        myClick(lc.loginButton);
+        mySendKeys(lc.email, "technodeneme007@gmail.com");
+        mySendKeys(lc.password, "12345Aa.");
+        myClick(lc.loginButton2);
+//        mySendKeys(hpc.searchBox, text + Keys.ENTER);
+        mySendKeys(hpc.searchBox, text);
+        myClick(hpc.searchButton);
+        Assert.assertEquals(hpc.productTitle.getText(), text, "The product title does not include the search term 'Adobe Photoshop CS4'.");
     }
 }
