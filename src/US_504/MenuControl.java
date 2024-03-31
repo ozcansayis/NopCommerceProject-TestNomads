b51183856bbd4275a0afd9cefabb21bc5feef966
Abdulkabir <turkogluabdulkabir@gmail.com>
2024-03-31 18:17:50 +0300

    menüler kontrol edildi

--- src/US_504/MenuControl.java
+++ src/US_504/MenuControl.java
@@ -1,10 +1,14 @@
 package US_504;
 
+import Utility.BaseDriverParameter;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
-public class Locatorlar {
-
+public class MenuControl {
+    public  MenuControl(){
+        PageFactory.initElements(BaseDriverParameter.driver, this);
+    }
     @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[1]")
     public WebElement Computers;
     @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]")
@@ -19,33 +23,38 @@ public class Locatorlar {
     public WebElement Jewelry;
     @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[7]")
     public WebElement GiftCards;
+    @FindBy(css = "div[class='center-2']>div>div>h1")
+    public WebElement Resalt;
+
 
     /////////////////////////////////////////////////////
     // alt menuler
     //Computers
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[1]/ul/li[1]")
+
+    @FindBy(xpath = "//a[text()=' Desktops ']")
     public WebElement Desktops;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[1]/ul/li[2]")
+    @FindBy(xpath = "//a[text()=' Notebooks ']")
     public WebElement Notebooks;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[1]/ul/li[3]")
+    @FindBy(xpath = "//a[text()=' Software ']")
     public WebElement Software;
 
 
     //Electronics
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li[1]")
+    @FindBy(xpath = "//a[text()=' Camera & photo ']")
     public WebElement CameraPhoto;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li[2]")
+    @FindBy(xpath = "//a[text()=' Cell phones ']")
     public WebElement CellPhones;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[2]/ul/li[3]")
+    @FindBy(xpath = "//a[text()=' Others ']")
     public WebElement Others;
 
 
     //Apparel
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li[1]")
+    @FindBy(xpath = "//a[text()=' Shoes ']")
     public WebElement Shoes;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li[2]")
+    @FindBy(xpath = "//a[text()=' Clothing ']")
     public WebElement Clothing;
-    @FindBy(xpath = "//ul[@class='top-menu notmobile']/li[3]/ul/li[3]")
+    @FindBy(xpath = "//a[text()=' Accessories ']")
     public WebElement Accessories;
+
 
 }
--- src/US_504/TabMenuControlTest.java
+++ src/US_504/TabMenuControlTest.java
@@ -1,10 +1,62 @@
 package US_504;
 
+import Utility.BaseDriverParameter;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class TabMenuControlTest {
+public class TabMenuControlTest extends BaseDriverParameter {
     @Test
     public void TC_504(){
+        MenuControl mc = new MenuControl();
+        myClick(mc.Computers);
+        Assert.assertTrue(mc.Resalt.getText().contains("Computers"));
+        myClick(mc.Desktops);
+        Assert.assertTrue(mc.Resalt.getText().contains("Desktops"));
+        driver.navigate().back();
+        myClick(mc.Notebooks);
+        Assert.assertTrue(mc.Resalt.getText().contains("Notebooks"));
+        driver.navigate().back();
+        myClick(mc.Software);
+        Assert.assertTrue(mc.Resalt.getText().contains("Software"));
+
+        myClick(mc.Electronics);
+        Assert.assertTrue(mc.Resalt.getText().contains("Electronics"));
+        myClick(mc.CameraPhoto);
+        Assert.assertTrue(mc.Resalt.getText().contains("Camera & photo"));
+        driver.navigate().back();
+        myClick(mc.CellPhones);
+        Assert.assertTrue(mc.Resalt.getText().contains("Cell phones"));
+        driver.navigate().back();
+        myClick(mc.Others);
+        Assert.assertTrue(mc.Resalt.getText().contains("Others"));
+
+        myClick(mc.Apparel);
+        Assert.assertTrue(mc.Resalt.getText().contains("Apparel"));
+        myClick(mc.Shoes);
+        Assert.assertTrue(mc.Resalt.getText().contains("Shoes"));
+        driver.navigate().back();
+        myClick(mc.Clothing);
+        Assert.assertTrue(mc.Resalt.getText().contains("Clothing"));
+        driver.navigate().back();
+        myClick(mc.Accessories);
+        Assert.assertTrue(mc.Resalt.getText().contains("Accessories"));
+
+
+        myClick(mc.Digitaldownloads);
+        Assert.assertTrue(mc.Resalt.getText().contains("Digital downloads"));
+
+        myClick(mc.Books);
+        Assert.assertTrue(mc.Resalt.getText().contains("Books"));
+
+        myClick(mc.Jewelry);
+        Assert.assertTrue(mc.Resalt.getText().contains("Jewelry"));
+
+        myClick(mc.GiftCards);
+        Assert.assertTrue(mc.Resalt.getText().contains("Gift Cards"));
+
+
+
 
     }
 }
